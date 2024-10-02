6d687fe0443e42c38688593ea0628312f4ca5cf3
fernandouski <fernandolopezchs@hotmail.com>
2024-10-02 11:56:47 -0700

    - making it responsive

--- app/dashboard/sign-up/index.tsx
+++ app/dashboard/sign-up/index.tsx
@@ -1,42 +1,55 @@
 import {SafeAreaView, View, StyleSheet, Dimensions} from "react-native";
 import {useForm, Controller, SubmitHandler} from 'react-hook-form';
 import ControlledTextInput from "@/components/FormControls/ControlledTextInput";
-import { Button } from 'react-native-paper'
+import {Button, Card} from 'react-native-paper'
 
 interface SignUpData {
     email: string;
     password: string;
 }
+
 export default function SignUp() {
-    const { control, handleSubmit, formState: { errors} } = useForm<SignUpData>();
+    const {control, handleSubmit, formState: {errors}} = useForm<SignUpData>();
     const onSubmit: SubmitHandler<SignUpData> = (data) => {
         console.log(data)
     }
 
     return (
-        <View style={styles.container}>
-            <SafeAreaView>
-                <ControlledTextInput<SignUpData> style={styles.formElement} control={control} label='Email' name={'email'}/>
-                <ControlledTextInput<SignUpData> style={styles.formElement} control={control} label='Password' name={'password'} secureTextEntry/>
-                <Button style={[styles.submitButton, styles.formElement]} onPress={handleSubmit(onSubmit)} >Submit</Button>
-            </SafeAreaView>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Card style={styles.card}>
+                <Card.Title title={'Sign-Up'}/>
+                <Card.Content>
+                    <ControlledTextInput<SignUpData> style={styles.formElement} control={control} label='Email'
+                                                     name={'email'}/>
+                    <ControlledTextInput<SignUpData> style={styles.formElement} control={control} label='Password'
+                                                     name={'password'} secureTextEntry/>
+                </Card.Content>
+                <Card.Actions>
+                    <Button style={[styles.submitButton]} onPress={handleSubmit(onSubmit)}>Sign-up</Button>
+                </Card.Actions>
+            </Card>
+        </SafeAreaView>
     )
 }
 
 
 const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: windowHeight * .30,
-        alignItems: 'center'
+        alignItems: 'center',
     },
     submitButton: {
         alignSelf: 'flex-end'
     },
     formElement: {
-        marginTop: 8
+        marginTop: 8,
+    },
+    card: {
+        maxWidth: 500,
+        width: '90%'
     }
 });
