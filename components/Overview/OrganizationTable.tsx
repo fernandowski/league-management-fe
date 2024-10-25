33b672610cb3007db69dacf82eb670940843aed3
fernandouski <fernandolopezchs@hotmail.com>
2024-10-25 09:50:57 -0700

    Adding organization cards

--- components/Overview/OrganizationTable.tsx
+++ components/Overview/OrganizationTable.tsx
@@ -1,11 +1,17 @@
 import {View} from "react-native";
 import {useOrganizationStore} from "@/stores/organizationStore";
+import {Card, Text} from "react-native-paper";
 
 export function OrganizationTable() {
     const {organizations} = useOrganizationStore();
     return (
-        <View>
-            
+        <View style={{paddingLeft: 8, paddingRight: 8}}>
+            {organizations.map(organization =>
+                <Card style={{marginTop: 8}} key={organization.id}>
+                    <Card.Content>
+                        <Text>{organization.name}</Text>
+                    </Card.Content>
+                </Card>)}
         </View>
     )
 }
