3184bb347cd61456a812cd32a82db02d8b40b2bf
fernandouski <fernandolopezchs@hotmail.com>
2024-10-31 10:57:41 -0700

    Adding form to create league.

--- components/League/LeagueList.tsx
+++ components/League/LeagueList.tsx
@@ -1,5 +1,5 @@
 import {leagues} from "@/components/League/LeagueOverview";
-import {Animated} from "react-native";
+import {Animated, StyleSheet, View} from "react-native";
 import ScrollView = Animated.ScrollView;
 import {Card, Text} from "react-native-paper";
 
@@ -10,16 +10,38 @@ interface LeagueListProps {
 export function LeagueList(props: LeagueListProps) {
     const leagueData: leagues[] = props.data
     return (
-        <ScrollView style={{paddingLeft: 8, paddingRight: 8}}>
-            {
-                leagueData.map((league: leagues) => (
-                    <Card style={{marginTop: 8}} key={league.id}>
-                        <Card.Content>
-                            <Text>{league.name}</Text>
-                        </Card.Content>
-                    </Card>
-                ))
-            }
-        </ScrollView>
+        <View style={[styles.outerContainer]}>
+            <View style={[styles.container]}>
+                <ScrollView>
+                    {
+                        leagueData.map((league: leagues) => (
+                            <Card style={{marginTop: 8}} key={league.id}>
+                                <Card.Content>
+                                    <Text>{league.name}</Text>
+                                </Card.Content>
+                            </Card>
+                        ))
+                    }
+                </ScrollView>
+            </View>
+        </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    outerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    container: {
+        flex: 1,
+        minWidth: '80%',
+        flexDirection: 'row',
+        marginTop: 16
+    },
+    addLeagueButton: {
+        alignSelf: 'flex-end'
+    }
+});
--- components/League/LeagueOverview.tsx
+++ components/League/LeagueOverview.tsx
@@ -1,7 +1,14 @@
 import {useOrganizationStore} from "@/stores/organizationStore";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {apiRequest} from "@/api/api";
 import {LeagueList} from "@/components/League/LeagueList";
+import {StyleSheet, View} from "react-native";
+import {Button, Modal, Text} from "react-native-paper";
+import ControlledTextInput from "@/components/FormControls/ControlledTextInput";
+import {useForm} from "react-hook-form";
+import {joiResolver} from "@hookform/resolvers/joi";
+import Joi from "joi";
+import {useFocusEffect} from "expo-router";
 
 
 export interface leagues {
@@ -16,29 +23,103 @@ export interface leagueResponse {
     team_ids: string[]
 }
 
+const schema = Joi.object({
+    name: Joi.string()
+        .required()
+        .messages({
+            "string.empty": "Organization name is required",
+        })
+});
+
+interface CreateLeagueData {
+    name: string
+}
+
 export default function LeagueOverview() {
     const {organization} = useOrganizationStore();
     const [leagues, setLeagues] = useState<leagues[]>([]);
-
-    useEffect(() => {
-        const fetchLeagues = async () => {
-            try {
-                const response = await apiRequest(`/v1/leagues/?organization_id=${organization}`, {method: 'GET'});
-                setLeagues(response.map((league: leagueResponse) => ({
-                    id: league.id,
-                    name: league.name,
-                    teamIds: league.team_ids
-                })));
-            } catch (e) {
-                setLeagues([])
-            }
+    const [showModal, setShowModal] = useState(false);
+    const {control, handleSubmit, formState: {errors}} = useForm<CreateLeagueData>(
+        {
+            resolver: joiResolver(schema),
         }
+    );
+    const handleOpenModal = () => {
+        setShowModal(!showModal);
+    };
 
-        fetchLeagues();
+    const fetchLeagues = useCallback(async () => {
+        try {
+            const response = await apiRequest(`/v1/leagues/?organization_id=${organization}`, {method: 'GET'});
+            setLeagues(response.map((league: leagueResponse) => ({
+                id: league.id,
+                name: league.name,
+                teamIds: league.team_ids
+            })));
+        } catch (e) {
+            setLeagues([]);
+        }
     }, [organization]);
+
+    const handleSave = async (data: CreateLeagueData) => {
+        try {
+            apiRequest('/v1/leagues', {
+                method: 'POST',
+                body: {
+                    name: data.name,
+                    organization_id: organization
+                }
+            })
+            fetchLeagues();
+            setShowModal(!showModal);
+        } catch (e) {
+
+        }
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchLeagues();
+            return () => {
+                setShowModal(false);
+            };
+        }, [fetchLeagues])
+    );
+
     return (
-        <>
-            {leagues.length <= 0 ? <></> : <LeagueList data={leagues}></LeagueList>}
-        </>
+        <View style={{flex: 1, marginTop: 16}}>
+            <Button style={[{alignSelf: 'flex-end'}]} mode={'elevated'} onPress={handleOpenModal}>+ Add League </Button>
+            <LeagueList data={leagues}></LeagueList>
+            <Modal visible={showModal} dismissable={false} contentContainerStyle={[styles.modal]}>
+                <View style={[styles.formContainer]}>
+                    <Text>Organization Name</Text>
+                    <ControlledTextInput label='Name' name={'name'} control={control} error={errors.name?.message}/>
+                    <Button style={{alignSelf: "flex-end"}} onPress={handleSubmit(handleSave)}>Save</Button>
+                </View>
+            </Modal>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 8,
+    },
+    modal: {
+        flex: 0.8,
+        padding: 16,
+        backgroundColor: "white",
+        width: "80%",
+        maxHeight: 400,
+        maxWidth: 400,
+        zIndex: 200,
+        alignSelf: "center"
+    },
+    formContainer: {
+        flex: 0.7,
+        padding: 16
+    }
+})
