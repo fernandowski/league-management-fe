27006a77d421089cac79880c5de494e53120faa0
fernandouski <fernandolopezchs@hotmail.com>
2024-11-19 10:29:32 -0800

    clean up

--- components/League/LeagueList.tsx
+++ components/League/LeagueList.tsx
@@ -1,23 +1,22 @@
-import {leagues} from "@/components/League/LeagueOverview";
-import {Animated, StyleSheet, View} from "react-native";
+import {Animated, View} from "react-native";
 import ScrollView = Animated.ScrollView;
 import {Button, Card, Text} from "react-native-paper";
-import id from "ajv/lib/vocabularies/core/id";
+import {League} from "@/hooks/useLeagueData";
 
 interface LeagueListProps {
-    data: leagues[],
+    data: League[],
     onPressInviteTeam: (id: string) => void
 }
 
 export function LeagueList(props: LeagueListProps) {
-    const leagueData: leagues[] = props.data
+    const leagueData = props.data
 
 
     return (
         <View style={{flex: 1}}>
                 <ScrollView>
                     {
-                        leagueData.map((league: leagues) => (
+                        leagueData.map((league) => (
                             <Card style={{marginBottom: 8, marginLeft: 1, marginRight: 1}} key={league.id}>
                                 <Card.Content>
                                     <Text>{league.name}</Text>
