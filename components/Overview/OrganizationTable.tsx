fc38c3b6f3e9d8357719a2b34287d6fc340c43b4
fernandouski <fernandolopezchs@hotmail.com>
2024-11-22 13:12:04 -0800

    centering  organization list.

--- components/Overview/OrganizationTable.tsx
+++ components/Overview/OrganizationTable.tsx
@@ -1,4 +1,4 @@
-import {Animated, View} from "react-native";
+import {Animated, StyleSheet, View} from "react-native";
 import {useOrganizationStore} from "@/stores/organizationStore";
 import {Card, Text} from "react-native-paper";
 import ScrollView = Animated.ScrollView;
@@ -6,13 +6,30 @@ import ScrollView = Animated.ScrollView;
 export function OrganizationTable() {
     const {organizations} = useOrganizationStore();
     return (
-        <ScrollView style={{paddingLeft: 8, paddingRight: 8}}>
-            {organizations.map(organization =>
-                <Card style={{marginTop: 8}} key={organization.id}>
-                    <Card.Content>
-                        <Text>{organization.name}</Text>
-                    </Card.Content>
-                </Card>)}
-        </ScrollView>
+        <View style={[styles.outerContainer]}>
+            <ScrollView style={[styles.viewContainer]}>
+                {organizations.map(organization =>
+                    <Card style={{marginTop: 8,}} key={organization.id}>
+                        <Card.Content>
+                            <Text>{organization.name}</Text>
+                        </Card.Content>
+                    </Card>)}
+            </ScrollView>
+        </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    outerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    viewContainer: {
+        flex: 1,
+        width: '80%',
+        paddingLeft: 8,
+        paddingRight: 8
+    }
+})
