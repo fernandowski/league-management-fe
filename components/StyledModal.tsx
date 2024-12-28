9e808ec43238bb63dbc24fbdf81b924178ac0483
fernandouski <fernandolopezchs@hotmail.com>
2024-12-28 12:48:06 -0800

    Adding props for width and height since this will be beneficial when dealing with different sizes of inputs.

--- components/StyledModal.tsx
+++ components/StyledModal.tsx
@@ -1,9 +1,11 @@
 import {Portal, Modal, Button} from "react-native-paper";
-import {View, StyleSheet} from "react-native";
+import {View, StyleSheet, DimensionValue} from "react-native";
 
 interface ModalProps {
     isOpen: boolean;
     children: React.ReactNode;
+    width?: DimensionValue;
+    height?: DimensionValue
 }
 
 export default function StyledModal(props: ModalProps) {
@@ -12,7 +14,11 @@ export default function StyledModal(props: ModalProps) {
             <Modal
                 visible={props.isOpen}
                 dismissable={false}
-                contentContainerStyle={styles.modal}>
+                contentContainerStyle={[
+                    styles.modal,
+                    props.width ? {width: props.width} : styles.defaultWidth,
+                    props.height ? {height: props.height} : styles.defaultHeight,
+                ]}>
                 <View style={styles.modalMainContent}>
                     {props.children}
                 </View>
@@ -23,14 +29,12 @@ export default function StyledModal(props: ModalProps) {
 
 const styles = StyleSheet.create({
     modal: {
-        width: "80%",
-        height: "80%",
         backgroundColor: "white",
         alignSelf: "center",
         borderRadius: 8,
         padding: 16,
         shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
+        shadowOffset: {width: 0, height: 2},
         shadowOpacity: 0.25,
         shadowRadius: 4,
         elevation: 5,
@@ -38,4 +42,10 @@ const styles = StyleSheet.create({
     modalMainContent: {
         flex: 1,
     },
+    defaultWidth: {
+        width: "80%",
+    },
+    defaultHeight: {
+        height: "80%",
+    }
 });
